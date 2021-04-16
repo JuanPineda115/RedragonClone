08374848249fb65b4e92a69dc6d510b2b68f0ec7
juan pablo <jupapineda@gmail.com>
2021-04-16 00:19:19 -0600

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # proyecto-final
 proyecto final de web, replica de la pagina de redragonla.com
+
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.js",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    filename: "index.html"
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates style nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            }
+          },
+        ],
+      }
+    ],
+  },
+};
